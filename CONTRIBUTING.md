98d903c0ea9b066a8f6da669e955a5b09e580485
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2025-07-16 01:13:30 +0530

    Test-ai

---
+++ CONTRIBUTING.md
@@ -0,0 +1,12 @@
+# if you want to Contributing to test.ai here is how u can do:-
+Thank you for your interest in contributing to our project! Here are some guidelines to help you get started.
+
+Steps to Contribute:
+
+1. Fork this repository.
+2. Create a new branch (`git checkout -b feature/YourFeature`).
+3. Make your changes and commit (`git commit -m 'Add new feature'`).
+4. Push to the branch (`git push origin feature/YourFeature`).
+5. Open a pull request.
+
+Thank you for your contributions!
---
+++ src/components/OurTeam.jsx
@@ -0,0 +1,27 @@
+
+"use client";
+import React from "react";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+const people = [
+  {
+    id: 1,
+    name: "Aniket Kumar",
+    designation: "Full Stack Developer",
+    image:
+      "https://avatars.githubusercontent.com/u/62953198?v=4",
+  },
+];
+
+export function OurTeamSection() {
+  return (
+    <div className="flex flex-col items-center justify-center mb-10 w-full">
+      <div className="font-normal text-2xl text-neutral-700 dark:text-neutral-400 max-w-lg">
+        My Profile
+      </div>
+      <div className="mt-4 flex flex-row items-center justify-center mb-10 w-full">
+        <AnimatedTooltip items={people} />
+      </div>
+    </div>
+  );
+}
+
